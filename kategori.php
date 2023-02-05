8c2953e8c0a7efee41844352578ef3566b8dd7b2
Charitha <92729642+CharithaRuwindu@users.noreply.github.com>
2023-02-06 01:23:08 +0530

    Update kategori.php

--- kategori.php
+++ kategori.php
@@ -199,7 +199,9 @@ $searchfor = strtolower(RemoveSpecialChar(base64_encode($_GET['data'])));
 
 <div class="pembungkus">
     
-   <font face="Century Gothic" color="white"><h3>&nbsp;&nbsp;kategori <?php echo $_GET['data'];?></h3></font>
+   <!-- <font face="Century Gothic" color="white"><h3>&nbsp;&nbsp;kategori <?php echo $_GET['data'];?></h3></font> -->
+    <font face="Century Gothic" color="white"><h3>&nbsp;&nbsp;kategori <?php echo htmlspecialchars($_GET['data'], ENT_QUOTES, 'UTF-8'); ?></h3></font>
+<ul class="nav" align="center">
 
 <ul class="nav" align="center">
 
